b334201316715fbec75c24b538f720947cb5c27d
John Griffiths <j.davidgriffiths@gmail.com>
2020-03-15 18:42:07 -0400

    ordered files list

--- code/compile_cpp_files.py
+++ code/compile_cpp_files.py
@@ -1,5 +1,5 @@
 import os,glob
-cpp_files = glob.glob('*.cpp')
+cpp_files = sorted(glob.glob('*.cpp'))
 for f in cpp_files:
   comp_str = 'g++ %s -o %s' %(f, f.split('.cpp')[0])
   print('compiling: %s' %comp_str)
